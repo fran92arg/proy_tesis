22012f80620be5a1f0a8d507d82a36aa95796e9e
floflor98 <flo.flor98@gmail.com>
2026-09-21 17:50:19 -0300

    borro eeglab

--- eeglab_current/eeglab/plugins/clean_rawdata/manopt/tests/test_fixedTTrank.m
+++
@@ -1,5 +0,0 @@
-clear all; close all; clc;
-
-M = fixedTTrankfactory([5 5 5], [1 5 5 1]);
-
-checkmanifold(M);
--- eeglab_current/eeglab/plugins/clean_rawdata/manopt/tests/test_powermanifold.m
+++
@@ -1,6 +0,0 @@
-clear all; close all; clc;
-
-S = spherefactory(5, 2);
-M = powermanifold(S, 7);
-checkmanifold(M);
-checkretraction(M);
