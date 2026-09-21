69988daf7ff159379d22f3ec3a3253ed93aff982
floflor98 <flo.flor98@gmail.com>
2026-09-21 17:45:59 -0300

    ...
    ..

---
+++ eeglab_current/eeglab/plugins/import_edf-eeglab_import_edf_v1.4/eegplugin_importedf.m
@@ -0,0 +1,49 @@
+% eegplugin_importedf() - EEGLAB plugin to import
+% 
+% Usage: eegplugin_importedf(fig, trystrs, catchstrs);
+%
+% Inputs:
+%   fig        - [integer]  EEGLAB figure
+%   trystrs    - [struct] "try" strings for menu callbacks.
+%   catchstrs  - [struct] "catch" strings for menu callbacks.
+%
+% Copyright (C) - Cedric Cannard, August 2022
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function vers = eegplugin_importedf(fig, trystrs, catchstrs)
+
+if nargin < 3
+    error('eegplugin_importedf requires 3 arguments');
+end
+
+% plugin version
+vers = 'import_edf1.4';
+
+% path
+if ~exist('import_edf.m','file')
+    addpath(genpath(fileparts(which('import_edf.m'))));
+end
+
+% Find menu
+menui = findobj(fig, 'tag', 'import data');
+
+% Menu callbacks
+comcnt = [trystrs.no_check '[EEG, LASTCOM] = import_edf();'  catchstrs.new_non_empty];
+
+% Create menus
+uimenu(menui, 'label', 'From EDF/EDF+ files (using MATLAB edfread)', 'separator', 'on', 'callback', comcnt);
+
+end
--- scripts/main.m
+++ scripts/main.m
@@ -15,115 +15,19 @@ eeglabRoot  = fullfile(raizProy, 'eeglab');
 datos = dir(fullfile(rutaData, '*.edf')); % con esto cargo los nombres de los .edf 
 % en una estructura
 %% Cargar
-EEG = pop_biosig('...\data\LUI14071945017');
+aux=strcat(rutaData,'\');
+aux=strcat(aux,datos.name);
+EEG = pop_biosig(aux);
 EEG=eeg_checkset(EEG);%verificar consistencia de la estructura
 f_muestreo=EEG.srate;
-%eeglab redraw
 %% butterworth pasaalto
 EEG=pasaalto(EEG);
-% t=EEG.times;
-% eeg_filt_hp=zeros(size(EEG.data));
-% fs=0.05; %frecuencia inicio banda de rechazo
-% fp=0.5; %frecuencia inicio banda de paso
-% ws=fs/(f_muestreo/2);
-% wp=fp/(f_muestreo/2);
-% [N,wn]=buttord(wp,ws,1,40); %%atenuación en la banda de paso y rechazo
-% [num,den]=butter(N,wn,'high'); %Coef de FT
-% figure(1)
-% title('Respuesta en frecuencia filtro pasaaltos')
-% freqz(num,den,2048,256);
-% figure(1)
-% title('Respuesta en frecuencia filtro pasaaltos')
-% freqz(pasaalto,2048,256);
-% for i=1:EEG.nbchan
-%     eeg_filt_hp(i,:)=filter(pasaalto,double(EEG.data(i,:))); 
-% end
- %figure(2);
-% %pwelch(eeg_filt_hp(5,:),hanning(1024),[], 2048,256); %senal de entrada, ventana(resolucion espectral o no me acuerdo),no superposiciónd de ventanas,cant de puntos,f muestreo señal
-% periodogram(eeg_filt_hp(5,:),[],1024,256)
-% hold on;
-% %pwelch(EEG.data(5,:),hanning(1024),[],2048,256);
-% periodogram(EEG.data(5,:),[],1024,256)
 
 %% Pasabajos
 EEG=pasabajo(EEG);
-% fs1=105;
-% fp1=100;
-% wspb=fs1/(f_muestreo/2);
-% wppb=fp1/(f_muestreo/2);
-% [N1,wn1]=buttord(wppb,wspb,1,10);
-% [num1,den1]=butter(N1,wn1,'low');
-
-% eeg_filt_lp=zeros(size(EEG.data));
-% figure(2)
-% title('Respuesta en frecuencia filtro pasabajos')
-% freqz(pasabajo,2048,256);
-% for i=1:EEG.nbchan
-%     eeg_filt_lp(i,:)=filter(pasabajo,double(eeg_filt_hp(i,:)));
-% end
-% figure(3);
-% %pwelch(eeg_filt_lp(5,:),hanning(1024),[], 2048,256); %señal de entrada, ventana(resolucion espectral o no me acuerdo),no superposiciónd de ventanas,cant de puntos,f muestreo señal
-% periodogram(eeg_filt_lp(5,:),[],1024,256)
-% 
-% hold on;
-% %pwelch(EEG.data(5,:),hanning(1024),[],2048,256);
-% periodogram(EEG.data(5,:),[],1024,256)
-
-%%
-% EEG.data= eeg_filt_lp;
-% EEG=pop_saveset(EEG,'moreno_filtradoPasaaltobajo.set','C:\Users\natyr\OneDrive\Escritorio')
 
 %% Filtro notch 50Hz
 EEG=notch(EEG);
-% flinea=50;
-% angulo=(flinea*90)/(f_muestreo/4);
-% z1=cos(angulo)+j*sin(angulo);
-% z2=conj(z1);
-% zpk([z1 z2],[],1)
-% num_ranura=[1 -(z1+z2) z1*z2]
-% den_ranura=1;
-%zplane(num_ranura,den_ranura);
-% BW     = 2;   % Bandwidth
-% Apass  = 1;   % Bandwidth Attenuation
-% 
-% [b, a] = iirnotch(Fnotch/(Fs/2), BW/(Fs/2), Apass);
-% Hd     = dfilt.df2(b, a);
-% eeg_filt_ranura=zeros(size(EEG.data));
-% for i=1:EEG.nbchan
-%     % eeg_filt_ranura(i,:)=filter(num_ranura,den_ranura,double(eeg_filt_lp(i,:)));
-%     eeg_filt_ranura(i,:)=filter(notch,double(eeg_filt_lp(i,:)));
-% end
-% figure(4)
-% freqz(notch)
-% title('Rta. módulo y fase - notch 50 Hz, fs=256 Hz')
-% figure(5);
-% %pwelch(eeg_filt_ranura(1,:),hanning(1024),[], 2048,256); %señal de entrada, ventana(resolucion espectral o no me acuerdo),no superposiciónd de ventanas,cant de puntos,f muestreo señal
-% periodogram(eeg_filt_ranura(1,:),[],1024,256)
-% hold on;
-% %pwelch(EEG.data(1,:),hanning(1024),[],2048,256);
-% periodogram(EEG.data(1,:),[],1024,256)
-
-%%
-% eeg_filt_ranura100=zeros(size(EEG.data));
-% for i=1:EEG.nbchan
-%     % eeg_filt_ranura(i,:)=filter(num_ranura,den_ranura,double(eeg_filt_lp(i,:)));
-%     eeg_filt_ranura100(i,:)=filter(notch100,double(eeg_filt_ranura(i,:)));
-% end
-% figure(5)
-% freqz(notch100)
-% title('Rta. módulo y fase - notch 100 Hz, fs=256 Hz')
-% figure(6);
-% %pwelch(eeg_filt_ranura100(1,:),hanning(1024),[], 2048,256); %señal de entrada, ventana(resolucion espectral o no me acuerdo),no superposiciónd de ventanas,cant de puntos,f muestreo señal
-% periodogram(eeg_filt_ranura100(1,:),[],1024,256)
-% 
-% hold on;
-% %pwelch(EEG.data(1,:),hanning(1024),[],2048,256);
-% periodogram(EEG.data(1,:),[],1024,256)
-
-%%
-% EEG.data= eeg_filt_ranura100;
-% EEG=eeg_checkset(EEG)
-% EEG=pop_saveset(EEG,'moreno_filtradonotch.set','C:\Users\natyr\OneDrive\Escritorio')
 
 % %% Cleanline
 % EEG = pop_cleanline(EEG, 'bandwidth', 1, ...
@@ -151,5 +55,6 @@ EEG=notch(EEG);
 
 %% ICA
 EEG=ICA(EEG)
-
+rutafiltrados= fullfile(raizProy,'data');
+EEG=por_writeeeg(EEG,rutafiltrados,'TYPE','EDF');
 
